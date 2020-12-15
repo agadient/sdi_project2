3f01c0834057ec1f6f5c2b3c4532d165ee480ee2
agadient <agadient@mit.edu>
2020-12-15 15:10:33 -0700

    add changes to controller and docker compose

---
+++ spring/src/main/java/com/example/demo/RegistrationController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+    @PostMapping("/new")
+    public String newRegistration() {
+        return "";
+    }
+}
