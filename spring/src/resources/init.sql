250d966f4213e9c76d93d9330b72471e48ccfc4e
agadient <agadient@mit.edu>
2020-12-15 15:18:15 -0700

    add sql file no uppercase

--- spring/src/resources/init.sql
+++ spring/src/resources/init.sql
@@ -1,46 +1,46 @@
-CREATE TABLE Locations (locationID serial, name text NOT NULL, PRIMARY KEY (locationID));
-INSERT INTO Locations(name) VALUES ('Kirtland');
-INSERT INTO Locations(name) VALUES ('Edwards');
-INSERT INTO Locations(name) VALUES ('Scranton');
-INSERT INTO Locations(name) VALUES ('Moscow');
+CREATE TABLE locations (location_id serial, name text NOT NULL, PRIMARY KEY (location_id));
+INSERT INTO locations(name) VALUES ('Kirtland');
+INSERT INTO locations(name) VALUES ('Edwards');
+INSERT INTO locations(name) VALUES ('Scranton');
+INSERT INTO locations(name) VALUES ('Moscow');
 
 
-CREATE TABLE Users (userID serial, username varchar(255) UNIQUE NOT NULL, passwordHash text NOT NULL,
-salt text, age int NOT NULL, locationID BIGINT UNSIGNED NOT NULL, PRIMARY KEY(userID), FOREIGN KEY (locationID) REFERENCES Locations(locationID));
+CREATE TABLE users (user_id serial, username varchar(255) UNIQUE NOT NULL, password_hash text NOT NULL,
+salt text, age int NOT NULL, location_id BIGINT UNSIGNED NOT NULL, PRIMARY KEY(user_id), FOREIGN KEY (location_id) REFERENCES locations(location_id));
 
-INSERT INTO Users(username, passwordHash, salt, age, locationID) VALUES ('fighterPilotXOXO', 'asdf', 'asdf', 28, 1);
-INSERT INTO Users(username, passwordHash, salt, age, locationID) VALUES ('crustyColonel', 'asdf', 'asdf', 55, 2);
-INSERT INTO Users(username, passwordHash, salt, age, locationID) VALUES ('MichaelScarn', 'asdf', 'asdf', 45, 3);
-INSERT INTO Users(username, passwordHash, salt, age, locationID) VALUES ('KarinaNotASpie', 'asdf', 'asdf', 20, 4);
-
-
-CREATE TABLE Hobbies (hobbyID serial, name text NOT NULL, PRIMARY KEY(hobbyID));
-
-INSERT INTO Hobbies(name) VALUES ('Flying');
-INSERT INTO Hobbies(name) VALUES ('Yelling');
-INSERT INTO Hobbies(name) VALUES ('Improv');
-INSERT INTO Hobbies(name) VALUES ('Spying');
-
-CREATE TABLE Conversations(conversationID serial, chatLog text NOT NULL, PRIMARY KEY(conversationID));
-
-INSERT INTO Conversations(chatLog) VALUES ('KarinaNotASpie: Hello handsome airmen. I like long walks on the beach and electronic schematics for the F-15E Strike Eagle. Wanna hang out?\r\n\r\nfighterPilotXOXO: Hey baby sure thing ever rode in an A-10 before?');
-INSERT INTO Conversations(chatLog) VALUES ('crustyColonel: Better cut your hair airmen!\r\n\r\n MichaelScarn: Yeesh');
+INSERT INTO users(username, password_hash, salt, age, location_id) VALUES ('fighterPilotXOXO', 'asdf', 'asdf', 28, 1);
+INSERT INTO users(username, password_hash, salt, age, location_id) VALUES ('crustyColonel', 'asdf', 'asdf', 55, 2);
+INSERT INTO users(username, password_hash, salt, age, location_id) VALUES ('MichaelScarn', 'asdf', 'asdf', 45, 3);
+INSERT INTO users(username, password_hash, salt, age, location_id) VALUES ('KarinaNotASpie', 'asdf', 'asdf', 20, 4);
 
 
-CREATE TABLE UserConversations (userConversationID serial, userID BIGINT UNSIGNED NOT NULL, conversationID BIGINT UNSIGNED NOT NULL,
-PRIMARY KEY(userConversationID), FOREIGN KEY (conversationID) REFERENCES Conversations(conversationID),
-FOREIGN KEY (userID) REFERENCES Users(userID));
+CREATE TABLE hobbies (hobby_id serial, name text NOT NULL, PRIMARY KEY(hobby_id));
 
-INSERT INTO UserConversations (userID, conversationID) VALUES (1, 1);
-INSERT INTO UserConversations (userID, conversationID) VALUES (4, 1);
-INSERT INTO UserConversations (userID, conversationID) VALUES (2, 2);
-INSERT INTO UserConversations (userID, conversationID) VALUES (3, 2);
+INSERT INTO hobbies(name) VALUES ('Flying');
+INSERT INTO hobbies(name) VALUES ('Yelling');
+INSERT INTO hobbies(name) VALUES ('Improv');
+INSERT INTO hobbies(name) VALUES ('Spying');
 
-CREATE TABLE UserHobbies(userHobbyID serial, hobbyID BIGINT UNSIGNED NOT NULL, userID BIGINT UNSIGNED NOT NULL,
-PRIMARY KEY (userHobbyID), FOREIGN KEY (hobbyID) REFERENCES Hobbies(hobbyID),
-FOREIGN KEY (userID) REFERENCES Users(userID));
+CREATE TABLE conversations(conversation_id serial, chatLog text NOT NULL, PRIMARY KEY(conversation_id));
 
-INSERT INTO UserHobbies (hobbyID, userID) VALUES (1, 1);
-INSERT INTO UserHobbies (hobbyID, userID) VALUES (2, 2);
-INSERT INTO UserHobbies (hobbyID, userID) VALUES (3, 3);
-INSERT INTO UserHobbies (hobbyID, userID) VALUES (4, 4);
+INSERT INTO conversations(chatLog) VALUES ('KarinaNotASpie: Hello handsome airmen. I like long walks on the beach and electronic schematics for the F-15E Strike Eagle. Wanna hang out?\r\n\r\nfighterPilotXOXO: Hey baby sure thing ever rode in an A-10 before?');
+INSERT INTO conversations(chatLog) VALUES ('crustyColonel: Better cut your hair airmen!\r\n\r\n MichaelScarn: Yeesh');
+
+
+CREATE TABLE user_conversations (user_conversation_id serial, user_id BIGINT UNSIGNED NOT NULL, conversation_id BIGINT UNSIGNED NOT NULL,
+PRIMARY KEY(user_conversation_id), FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id));
+
+INSERT INTO user_conversations (user_id, conversation_id) VALUES (1, 1);
+INSERT INTO user_conversations (user_id, conversation_id) VALUES (4, 1);
+INSERT INTO user_conversations (user_id, conversation_id) VALUES (2, 2);
+INSERT INTO user_conversations (user_id, conversation_id) VALUES (3, 2);
+
+CREATE TABLE user_hobbies(user_hobby_id serial, hobby_id BIGINT UNSIGNED NOT NULL, user_id BIGINT UNSIGNED NOT NULL,
+PRIMARY KEY (user_hobby_id), FOREIGN KEY (hobby_id) REFERENCES hobbies(hobby_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id));
+
+INSERT INTO user_hobbies (hobby_id, user_id) VALUES (1, 1);
+INSERT INTO user_hobbies (hobby_id, user_id) VALUES (2, 2);
+INSERT INTO user_hobbies (hobby_id, user_id) VALUES (3, 3);
+INSERT INTO user_hobbies (hobby_id, user_id) VALUES (4, 4);
